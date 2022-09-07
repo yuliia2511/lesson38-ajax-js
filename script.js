370b3340f85cd6ce1ee3b1d96ff8312753d6349b
yuliia2511 <yuliyaaa.katanchyk@gmail.com>
2022-09-07 12:56:33 -0300

    fix search result

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-document.getElementById('form').addEventListener('submit', (element) => {
-    element.preventDefault();
-    searchContent(document.querySelector('input[name="content"]').value);
-});
+const titleInput = document.getElementById('title');
+const submitBtn = document.getElementById('btn');
+const prom = document.getElementById('prom');
 
-function searchContent() {
-    const name = document.getElementById('title');
+function searchResult(title, source) {
+    let blockTitle = document.createElement('div');
+    blockTitle.setAttribute('class', 'search-title');
+    blockTitle.innerText +=  `${title}`;
 
+    prom.append(blockTitle);
+    let blockImg = document.createElement('img');
 
-    const omdbApi = new XMLHttpRequest();
-    const omdbUrl =  `http://www.omdbapi.com/&apikey=862178f8=${name}&type=movie`;
-    omdbApi.open('get', omdbUrl, true);
-
-    omdbApi.onload = function(e) {
-        e.preventDefault();
-
-        if(this.status == 200) {
-            const result = JSON.parse(this.responseText);
-
-            console.log(result);
-
-            const outputResult = '';
-            for(const i in result) {
-                outputResult +=
-                '<div class="item">' +
-                '<p>Title:' + result.Title + '</p>' +
-                '</div>'
-            }
-            document.getElementById('search-result').innerText = outputResult;
-        } else {
-            alert('Movie not found!');
-        }
+    if (source == 'N/A') {
+        blockImg.setAttribute('src', `/img/ilustracao-de-pagina-de-erro-web.jpeg`);
+    } else {
+        blockImg.setAttribute('src', `${source}`);
     }
-    omdbApi.send(); //The 400 (Bad Request) status code - (ошибка клиента; узнать как исправить)
+
+    prom.append(blockImg);
 }
 
+function createRequest(method, url) {
+    return fetch(url).then(response => {
+        return response.json();
+    });
+}
+
+function submitRequest(element) {
+    element.preventDefault();
+
+    let s = `${titleInput.value}`;
+    const requestItem = `https://www.omdbapi.com/?apikey=862178f8&s=${titleInput.value}`;
+
+    createRequest('GET', requestItem).then(data => {
+        JSON.stringify(data.Search.forEach(element => {
+            searchResult(element.Title, element.Poster);
+        }));
+    });
+}
+
+submitBtn.addEventListener('click', submitRequest);
 
 
 
@@ -42,6 +47,38 @@ function searchContent() {
 
 
 
+// document.getElementById('form').addEventListener('submit', (element) => {
+//     element.preventDefault();
+//     searchContent(document.querySelector('input[name="content"]').value);
+// });
+
+// function searchContent() {
+//     const name = document.getElementById('title');
+
+//     const omdbApi = new XMLHttpRequest();
+//     const omdbUrl =  `https://www.omdbapi.com/?apikey=862178f8&i=tt3896198&name=${name}`;
+//     omdbApi.open('get', omdbUrl, true);
+
+//     omdbApi.onload = function(e) {
+//         e.preventDefault();
+
+//         if(this.status == 200) {
+//             const result = JSON.parse(this.responseText);
+
+//             const outputResult = '';
+//             for(const i in result) {
+//                 outputResult ==
+//                 '<div class="item">' +
+//                 '<p>Title:' + result.Title + '</p>' +
+//                 '</div>'
+//             }
+//             document.getElementById('search-result').innerText = outputResult;
+//         } else {
+//             alert('Movie not found!');
+//         }
+//     }
+//     omdbApi.send(); 
+// }
 
 
 
